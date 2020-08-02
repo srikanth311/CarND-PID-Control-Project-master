5bff0af8a118b3d6ca2891387219106114e2c693
Srikanth Kodali <skkodali@amazon.com>
2020-08-01 22:59:05 -0500

    latest

---
+++ src/PID.cpp
@@ -0,0 +1,37 @@
+#include "PID.h"
+
+/**
+ * TODO: Complete the PID class. You may add any additional desired functions.
+ */
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp_, double Ki_, double Kd_) {
+  /**
+   * TODO: Initialize PID coefficients (and errors, if needed)
+   */
+
+  this->Kp = Kp_; this->Ki = Ki_; this->Kd = Kd_;
+  this->p_error = 0;
+  this->i_error = 0;
+  this->d_error = 0;
+}
+
+void PID::UpdateError(double cte) {
+  /**
+   * TODO: Update PID errors based on cte.
+   */
+   d_error = cte - p_error;
+   p_error = cte;
+   i_error = i_error + cte;
+}
+
+double PID::TotalError() {
+  /**
+   * TODO: Calculate and return the total error
+   */
+  // return 0.0;  // TODO: Add your total error calc here!
+  return (-Kp * p_error) - (Kd * d_error) - (Ki * i_error);
+}
